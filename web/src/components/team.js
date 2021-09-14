4f5565f5ad58196a325fc2e0c520b0e296fabdff
Artur Pabian <spistel@gmail.com>
2021-09-14 02:11:25 +0200

    - read content teams from MDX files

--- web/src/components/team.js
+++ web/src/components/team.js
@@ -1,29 +1,67 @@
 import React from "react"
 import { Link } from "gatsby"
-// import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+
+const query = graphql`
+{
+  allMdx(filter: {slug: {eq: "sections/4-Team.en"}}) {
+    nodes {
+      frontmatter {
+        teamMember {
+          header
+          imageFileName
+          subheader
+          social {
+            twitter
+            facebook
+            linkedin
+            medium
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 const Team = () => {
-//   const data = useStaticQuery(graphql`
-//   {
-//     allMdx(filter: {slug: {eq: "sections/4-Team.en"}}) {
-//       nodes {
-//         frontmatter {
-//           teamMember {
-//             header
-//             imageFileName
-//             subheader
-//             social {
-//               twitter
-//               facebook
-//               linkedin
-//               medium
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `)
+
+    const data = useStaticQuery(query)
+    const teams = data.allMdx.nodes[0].frontmatter.teamMember.map(team => 
+        <>
+            <div className="col-lg-6 col-md-6 wow move-up animated">
+                <div className="grid-item">
+                    <div className="ht-team-member">
+                        <div className="team-image">
+                            <img className="img-fluid" src="assets/images/2.webp" alt=""/>
+                            <div className="social-networks">
+                                <div className="inner">
+                                    <Link target="_blank" to={team.social['facebook']}
+                                        className=" hint--bounce  hint--top hint--theme-two"
+                                        aria-label="Facebook"><i className="fab fa-facebook"></i>
+                                    </Link>
+                                    <Link target="_blank" to={team.social['twitter']}
+                                        className=" hint--bounce hint--top hint--theme-two"
+                                        aria-label="Twitter"><i className="fab fa-twitter"></i>
+                                    </Link>
+                                    <Link target="_blank" to={team.social['linkedin']}
+                                        className=" hint--bounce hint--top hint--theme-two"
+                                        aria-label="linkedin"><i className="fab fa-linkedin"></i>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team-info text-center">
+                            <h5 className="name">{team.header}</h5>
+                            <div className="position">{team.subheader}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+
   return (
       <>
           <div id="team" className="section-space--ptb_100 infotechno-section-bg-01">
@@ -38,122 +76,7 @@ const Team = () => {
                             </div>
                         </div>
                         <div className="row ht-team-member-style-two">
-                            <div className="col-lg-6 col-md-6 wow move-up animated">
-                                <div className="grid-item">
-                                    <div className="ht-team-member">
-                                        <div className="team-image">
-                                            <img className="img-fluid" src="assets/images/2.webp" alt=""/>
-                                            <div className="social-networks">
-                                                <div className="inner">
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce  hint--top hint--theme-two"
-                                                        aria-label="Facebook"><i className="fab fa-facebook"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Twitter"><i className="fab fa-twitter"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Instagram"><i className="fab fa-instagram"></i>
-                                                    </Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="team-info text-center">
-                                            <h5 className="name">Arkadiusz</h5>
-                                            <div className="position">Lead Designer</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 wow move-up animated">
-                                <div className="grid-item">
-                                    <div className="ht-team-member">
-                                        <div className="team-image">
-                                            <img className="img-fluid" src="assets/images/2.webp" alt=""/>
-                                            <div className="social-networks">
-                                                <div className="inner">
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce  hint--top hint--theme-two"
-                                                        aria-label="Facebook"><i className="fab fa-facebook"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Twitter"><i className="fab fa-twitter"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Instagram"><i className="fab fa-instagram"></i>
-                                                    </Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="team-info text-center">
-                                            <h5 className="name">Borysław</h5>
-                                            <div className="position">Lead Designer</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 wow move-up animated">
-                                <div className="grid-item">
-                                    <div className="ht-team-member">
-                                        <div className="team-image">
-                                            <img className="img-fluid" src="assets/images/2.webp" alt=""/>
-                                            <div className="social-networks">
-                                                <div className="inner">
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce  hint--top hint--theme-two"
-                                                        aria-label="Facebook"><i className="fab fa-facebook"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Twitter"><i className="fab fa-twitter"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Instagram"><i className="fab fa-instagram"></i>
-                                                    </Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="team-info text-center">
-                                            <h5 className="name">Przemysław</h5>
-                                            <div className="position">Lead Designer</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 wow move-up animated">
-                                <div className="grid-item">
-                                    <div className="ht-team-member">
-                                        <div className="team-image">
-                                            <img className="img-fluid" src="assets/images/2.webp" alt=""/>
-                                            <div className="social-networks">
-                                                <div className="inner">
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce  hint--top hint--theme-two"
-                                                        aria-label="Facebook"><i className="fab fa-facebook"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Twitter"><i className="fab fa-twitter"></i>
-                                                    </Link>
-                                                    <Link target="_blank" href="#"
-                                                        className=" hint--bounce hint--top hint--theme-two"
-                                                        aria-label="Instagram"><i className="fab fa-instagram"></i>
-                                                    </Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="team-info text-center">
-                                            <h5 className="name">Patryk</h5>
-                                            <div className="position">Lead Designer</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            {teams}
                         </div>
                     </div>
                 </div>
